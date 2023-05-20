93726700aa661a89a9257c4436b839a9f6902b49
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-05-20 18:39:57 +0530

    Spring boot learning

---
+++ deb-spring-boot-app/01-dev-spring-boot/myapp/src/main/java/com/love2learn/springboot/myapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.love2learn.springboot.myapp.rest;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello world"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "I love you Sweety";
+    }
+}
