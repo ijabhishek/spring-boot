55c7d7f4ed6a25000a0a51a82d011bc24b50afe7
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-05-22 20:05:16 +0530

    Add files via upload

---
+++ 01-cunstructor-injection/02-component-scanning/src/main/java/com/pyaribindu/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.pyaribindu.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 01-cunstructor-injection/02-component-scanning/src/test/java/com/pyaribindu/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyaribindu.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
